88eb75340a6919c2fa884115c57a411abfd4b41a
Jhorel Candia <jhorelcandia5csa@gmail.com>
2026-05-29 18:24:22 -0400

    Avance HU-19 mensajeria

---
+++ app/HU-19/ListaMensajes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ListaMensajes() {
+  return (
+    <View>
+      <Text>Lista de Mensajes</Text>
+    </View>
+  );
+}
